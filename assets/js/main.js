5f34749efc5bea326388478c54fd133e210e2f22
Gabriele Guo <orphenaruto@hotmail.it>
2020-10-28 15:08:36 +0100

    deleted not working loading screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-$(window).on("load",function(){
-	$(".loader-wrapper").fadeOut("slow");
-  });
-
 (function ($) {
 
 	var $window = $(window),
